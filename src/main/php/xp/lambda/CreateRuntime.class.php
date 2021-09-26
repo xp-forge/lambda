23b07319abd866f333f0aff7109ecc4404cd4eaa
Timm Friebe <thekid@thekid.de>
2021-09-26 12:57:08 +0200

    Refactor container execution to Process API provided by XP 10.14

--- src/main/php/xp/lambda/CreateRuntime.class.php
+++ src/main/php/xp/lambda/CreateRuntime.class.php
@@ -15,7 +15,6 @@ class CreateRuntime {
   }
 
   public function run(): int {
-    $docker= $this->command();
 
     // Build test and runtime containers when -b is given
     if ($this->rebuild) {
@@ -29,20 +28,20 @@ class CreateRuntime {
     $code= "echo ' => PHP ', PHP_VERSION, ' & Zend ', zend_version(), ' @ ', php_uname(), PHP_EOL, ' => ', implode(' ', get_loaded_extensions()), PHP_EOL;";
     Console::writeLine();
     Console::writeLine("[+] Running {$runtime}\e[34m");
-    passthru("{$docker} run --rm {$runtime} /opt/php/bin/php -r \"{$code}\"", $result);
+    $this->passthru(['run', '--rm', $runtime, '/opt/php/bin/php', '-r', $code]);
     Console::writeLine("\e[0m");
 
     // Extract runtime
     $container= uniqid();
     $commands= [
-      "{$docker} create --name {$container} {$runtime}",
-      "{$docker} cp {$container}:/opt/php/runtime.zip {$this->target}",
-      "{$docker} rm -f {$container}",
+      ['create', '--name', $container, $runtime],
+      ['cp', "{$container}:/opt/php/runtime.zip", $this->target],
+      ['rm', '-f', $container],
     ];
     Console::writeLine('[+] Creating ', $this->target);
     foreach ($commands as $command) {
-      Console::writeLinef("\e[34m => %s\e[0m", $command);
-      exec($command, $out, $result);
+      Console::writeLinef("\e[34m => %s\e[0m", implode(' ', $command));
+      $result= $this->passthru($command);
     }
 
     Console::writeLine();
